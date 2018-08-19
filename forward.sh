d4755bf776ef49e8d02188dc29766ad98e559b4e
UmutAlihan <alihandikel@gmail.com>
2018-08-19 00:11:54 +0000

    minor changes

--- forward.sh
+++ forward.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ssh -p 37212 -nNTv -R 0.0.0.0:2099:localhost:3002 -R 0.0.0.0:2098:localhost:3003 uad@128.199.50.73
+ssh -p 37212 -nNTvvv -R 0.0.0.0:2099:localhost:3002 -R 0.0.0.0:2098:localhost:3003 uad@128.199.50.73
 
 
 #port_t @Touch.html
