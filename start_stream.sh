dfbb6e85f979a12d3a5c86788ff1765097e8d78c
UmutAlihan <alihandikel@gmail.com>
2018-10-03 08:12:19 +0000

    minor fix, key-events url added

--- start_stream.sh
+++ start_stream.sh
@@ -1,3 +1,3 @@
-LD_LIBRARY_PATH=/opt/mjpg-streamer/ /opt/mjpg-streamer/mjpg_streamer -i "input_raspicam.so -fps 4 -q 5 -x 640 -y 480" -o "output_http.so -p 3002 -w /opt/mjpg-streamer/www" > /dev/null 2>&1&
+LD_LIBRARY_PATH=/opt/mjpg-streamer/ /opt/mjpg-streamer/mjpg_streamer -i "input_raspicam.so -fps 4 -q 4 -x 640 -y 480" -o "output_http.so -p 3002 -w /opt/mjpg-streamer/www" > /dev/null 2>&1&
 
 #-vf -hf  ->-fps'den önceki arg
